cd443d346d4907f0f3130e4d26a840561f4ad729
jerin <jerinj447@gmail.com>
2022-03-02 17:46:23 +0530

    building frontend

--- NUBI/NUBI/urls.py
+++ NUBI/NUBI/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('second/', views.index1, name='index1')
+
 ]
